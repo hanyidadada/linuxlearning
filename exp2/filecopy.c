8ebf2372bf326d0142d8c49f77a8b9f24f401ac4
huawei-cloud <1623298264@huawei.com>
2021-10-18 16:29:36 +0800

    linux 实验

---
+++ exp2/filecopy.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <libgen.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+
+int main(int argc, char *argv[]){
+    int srcfd, dstfd;
+    struct stat srcstat, dststat;
+    char ch;
+    char *srcfilename;
+    char destname[128];
+    char *srcbuffer, *dstbuffer;
+    pid_t pid;
+    int n = 5;
+    int size, ret, i; 
+    if (argc < 3) {
+        perror("./run sourcfile dest_diretory");
+        exit(-1);
+    }
+    
+    ret = access(argv[1], F_OK);
+    if (ret < 0) {
+        perror("source file doesn't exit");
+        exit(-1);
+    } 
+
+    srcfd = open(argv[1], O_RDONLY);
+    if (srcfd < 0) {
+        perror("open sourcefile wrong!");
+        exit(-1);
+    }
+    ret = fstat(srcfd, &srcstat);
+    if (ret < 0) {
+        perror("fstat wrong!");
+        exit(-1);
+    }
+    srcbuffer = mmap(NULL, srcstat.st_size, PROT_READ, MAP_SHARED, srcfd, 0);
+    if (srcbuffer == MAP_FAILED) {
+        perror("mmap src file failed:");
+        exit(-1);
+    }
+    close(srcfd);
+    
+    srcfilename = (char *) basename(argv[1]);
+    strcpy(destname, argv[2]);
+    stat(destname, &dststat);
+    if(S_ISDIR(dststat.st_mode)) {
+        if (destname[strlen(destname)-1] == '/') {
+            strcat(destname, srcfilename);
+        } else {
+            strcat(destname, "/");
+            strcat(destname, srcfilename);
+        }
+    }
+    
+    ret = access(destname, F_OK);
+    if (ret == 0) {
+        printf("source file is already exit! should I overwrite it?(y or n)");
+        scanf(" %c", &ch);
+        if(ch == 'n') {
+            exit(0);
+        }
+    }
+    dstfd = open(destname, O_RDWR|O_CREAT, 0644);
+    if (dstfd < 0) {
+        perror("open destfile wrong!");
+        exit(-1);
+    }
+    ftruncate(dstfd, srcstat.st_size);
+    dstbuffer = mmap(NULL, srcstat.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, srcfd, 0);
+    if (dstbuffer == MAP_FAILED) {
+        perror("mmap src file failed:");
+        exit(-1);
+    }
+    close(dstfd);
+    for (i = 0; i < n; i++) {
+        pid = fork();
+        if (pid < 0) {
+            perror("fork error:");
+            exit(-1);
+        } else if (pid == 0) {
+            size = srcstat.st_size / n;
+            if (i == n-1) {
+                memcpy(dstbuffer + i * size, srcbuffer+i * size, size + (srcstat.st_size % n));
+            } else {
+                memcpy(dstbuffer + i * size, srcbuffer+i * size, size);
+            }
+            exit(0);;
+        } else {
+            continue;
+        }
+    }
+    for (i = 0; i < n; i++) {
+        wait(NULL);
+    }
+    munmap(dstbuffer, srcstat.st_size);
+    munmap(srcbuffer, srcstat.st_size);
+    exit(0);
+}
---
+++ exp4/copydir.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include <dirent.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+void copydir(DIR *dir, const char workdir[]);
+
+int main(int argc, char *argv[]){
+    int i, n;
+    DIR *dir;
+    char workdir[128];
+    if (argc > 2) {
+        perror("too many argv!");
+        exit(-1);
+    }
+    if (argc == 2) {
+        strcpy(workdir, argv[1]);
+    } else {
+        if(getcwd(workdir, 128) == NULL) {
+            perror("get current work dir error");
+            exit(-1);
+        }
+    }
+    printf("\t%s\n", workdir);
+    dir = opendir(workdir);
+    copydir(dir, workdir);
+    closedir(dir);
+    printf("copy end\n");
+    return 0;
+}
+
+void copydir(DIR *dir, const char workdir[])
+{
+    struct dirent *currentdp = NULL;
+    char filepath[128];
+    struct stat fstat;
+    pid_t pid;
+    
+    while((currentdp = readdir(dir))!= NULL) {
+        memset(filepath, 0, 128);
+        strcpy(filepath, workdir);
+        strcat(filepath, "/");
+        strcat(filepath, currentdp->d_name);
+        lstat(filepath, &fstat);
+        if (!strcmp(currentdp->d_name, ".") || !strcmp(currentdp->d_name, "..")) {
+            continue;
+        }
+                
+        if(!S_ISDIR(fstat.st_mode)){
+            pid = fork();
+            if (pid < 0) {
+                perror("fork error");
+                exit(-1);
+            } else if (pid == 0) {
+                execl("/root/linuxexp/exp2/filecopy", ".filecopy", filepath, "/home/ywj/", NULL);
+                exit(0);
+            } else {
+                waitpid(pid, NULL, 0);
+                printf("copy %s end\n", filepath);
+            }
+        } else {
+            pid = fork();
+            if (pid < 0) {
+                perror("fork error");
+                exit(-1);
+            } else if (pid == 0) {
+                execlp("./copydir", "./copydir", filepath, NULL);
+                exit(0);
+            } else {
+                waitpid(pid, NULL, 0);
+            }
+        }
+    }
+}
+
